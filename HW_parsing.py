e9ac3178412c522e221e34fc7c32096042ace0d6
Roman Smolyakov <123468411+Roman9456@users.noreply.github.com>
2023-08-04 17:06:46 +0300

    Update HW_parsing.py

--- HW_parsing.py
+++ HW_parsing.py
@@ -1,34 +1,36 @@
-import time
+import requests
 from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from fake_useragent import UserAgent
 import json
 
 def parse_vacancies(url, params):
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    driver = webdriver.Chrome(options=chrome_options)
+    ua = UserAgent()
+    headers = {"user-agent": ua.random}
 
-    try:
-        response = driver.get(url)
-        print("Server response status code:", response)
+    response = requests.get(url, headers=headers, params=params)
+    print("Server response status code:", response.status_code)
+    print("Server response content:")
+    print(response.text)
 
-        
-        time.sleep(10)
-        
-        soup = BeautifulSoup(driver.page_source, 'html.parser')
-
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
         vacancies = []
 
-        vacancy_items = soup.find_all('div', {'class': 'vacancy-serp-item'})
+        vacancy_items = soup.find_all('a', {'data-qa': 'serp-item__title'})
         print("Number of vacancy items found:", len(vacancy_items))
 
         for vacancy_item in vacancy_items:
-            vacancy_title = vacancy_item.find('a', {'data-qa': 'vacancy-serp__vacancy-title'}).text
-            vacancy_link = vacancy_item.find('a', {'data-qa': 'vacancy-serp__vacancy-title'}).get('href')
-            company_name = vacancy_item.find('a', {'data-qa': 'vacancy-serp__vacancy-employer'}).text
-            city = vacancy_item.find('span', {'data-qa': 'vacancy-serp__vacancy-address'}).text
-            salary_element = vacancy_item.find('span', {'data-qa': 'vacancy-serp__vacancy-compensation'})
+            vacancy_title = vacancy_item.text
+            vacancy_link = vacancy_item.get('href')
+            
+            company_element = vacancy_item.find_next('a', {'data-qa': 'vacancy-serp__vacancy-employer'})
+            company_name = company_element.text if company_element else ""
+            
+            address_element = vacancy_item.find_next('div', {'data-qa': 'vacancy-serp__vacancy-address'})
+            address_text = address_element.text if address_element else ""
+            city, metros = extract_city_and_metro(address_text, soup)
+            
+            salary_element = vacancy_item.find_next('span', {'data-qa': 'vacancy-serp__vacancy-compensation'})
             salary = salary_element.text.strip() if salary_element else ""
 
             if 'Python' in vacancy_title and ('Django' in vacancy_title or 'Flask' in vacancy_title):
@@ -37,12 +39,29 @@ def parse_vacancies(url, params):
                     'link': vacancy_link,
                     'company': company_name,
                     'city': city,
+                    'metros': metros,
                     'salary': salary
                 })
 
         return vacancies
-    finally:
-        driver.quit()
+    else:
+        print("Failed to get data from the page.")
+        return []
+
+def extract_city_and_metro(address_text, soup):
+    city = ""
+    metros = []
+
+    address_parts = address_text.split(',')
+    if len(address_parts) >= 1:
+        city = address_parts[0].strip()
+
+    metro_elements = soup.select('span.metro-station')
+    for metro_element in metro_elements:
+        metro_name = metro_element.text.strip()
+        metros.append(metro_name)
+
+    return city, metros
 
 if __name__ == "__main__":
     url = "https://spb.hh.ru/search/vacancy"
@@ -57,6 +76,9 @@ if __name__ == "__main__":
 
     with open('vacancies.json', 'w', encoding='utf-8') as file:
         json.dump(vacancies, file, indent=2, ensure_ascii=False)
+
+    print("Vacancies have been successfully parsed and saved to vacancies.json.")
+
 
     print("Vacancies have been successfully parsed and saved to vacancies.json.")
 
